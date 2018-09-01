e7e9cefba42bc73ec38f021fee75d5cc1413203e
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2018-09-01 02:56:42 +0000

    setup games model

--- app/controllers/api/games_controller.rb
+++ app/controllers/api/games_controller.rb
@@ -1,11 +1,33 @@
 class Api::GamesController < ApplicationController
+    
+    def create
+        render json: {id: new_game.id}, status: 201
+    end
+    
     def show
-         game_hash = {
+        game_hash = {
           score: 150,
           score_by_frame: 7,
          
         }
         render json: game_hash, status: 200
+    end
+    
+    
+    private
+
+    def new_game
+        @new_game =  @new_game || Game.create
+    end
+    
+    def game
+        @game = @game || Game.cached_find_by_id(params[:id])
+    end
+    
+    def update_params
+        raise(ActionController::ParameterMissing, "Wrong knocked pins data format. Please input a valid number.") if params[:knocked_pins].to_s.chars.any? {|c| c=~/[^\d]/}
+        params.require(:knocked_pins).permit(:knocked_pins)
+        # params.require(:person).permit(:name, :age)
     end
     
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,98 @@
 class Game < ApplicationRecord
+    PINS = 10
+    FRAMES = 10
+
+    serialize :frames, Array
+
+    before_save do
+        self.score = frames.flatten.sum
+        self.frames = [[]] if frames.empty?
+        self.remove_from_cache
+    end
+    
+    include CachedFindById
+    
+    def throw!(knocked_pins)
+        self.transaction do
+          self.lock!
+          raise(GameError, "This game is over.") if game_over?
+          raise(AvailablePinsError, "Can't knock more pins than available.") unless knocked_pins.between?(0, avaliable_pins)
+          frames.last << knocked_pins
+          fill_older_open_frames_with knocked_pins
+          frames << [] if frame_completed?(frames.last) && !game_over?
+          save!
+        end
+    end
+    
+    def game_over?
+        frames.size == FRAMES && frame_completed?(frames.last)
+    end
+    
+    private
+    def fill_older_open_frames_with(value)
+        frames.map do |frame|
+            next if frame.equal?(frames.last)
+            frame << value if strike?(frame) && frame.size <= 2 #strike
+            frame << value if spare?(frame) && frame.size == 2 #spare
+        end
+    end
+    
+    def strike?(frame)
+        frame[0] === PINS
+    end
+
+    def double_strike?(frame)
+        frame[0] === PINS && frame[1] === PINS
+    end
+    
+    def spare?(frame)
+        frame[0] + frame[1] === PINS
+    end
+    
+    def ending_frame?(frame)
+        frames.size === FRAMES && frames.last.equal?(frame)
+    end
+    
+    def frame_completed?(frame)
+        # return ending_frame_completed?(frame) if ending_frame?(frame)
+        # return true if frame.nil? || frame.size===2 || strike?(frame)
+        # false
+        if strike?(frame) || spare?(frame)
+            if frame.size === 3
+                return true
+            end 
+        else 
+            if frame.size === 2
+                return true
+            end 
+        end 
+        return false
+    end
+    
+    def ending_frame_completed?(frame)
+        if (strike?(frame) || spare?(frame))
+          frame.size === 3
+        else
+          frame.size === 2
+        end
+    end
+    
+    def avaliable_pins
+        current_frame = frames.last
+        current_frame_score = current_frame.to_a.sum
+        if ending_frame?(current_frame)
+          return (PINS*3 - current_frame_score) if double_strike?(current_frame)
+          return (PINS*2 - current_frame_score) if strike?(current_frame) || spare?(current_frame)
+        end
+        PINS - current_frame_score
+    end
+    
+end
+
+
+
+class GameError < StandardError
+end
+
+class AvailablePinsError < StandardError
 end
